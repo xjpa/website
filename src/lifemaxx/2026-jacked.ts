4451c706f8b8e908e3210b2d28810b168af342a8
xjpa <xjpa@proton.me>
2026-05-12 16:32:39 +0800

    update

--- src/lifemaxx/2026-jacked.ts
+++ src/lifemaxx/2026-jacked.ts
@@ -9,14 +9,14 @@ const entry: LifemaxxEntry = {
   content: `
     <h2>march 5, 2026</h2>
     <section>
-      <p>i look meh, poverty abs.</p>
+      <p>the following is a pic of me</p>
       <img src="/images/lifemaxx/2026/physique/physique.jpeg" alt="mirror selfie of me" />
-      <p>i barely workout, 2x a week max, but often its more like once a week.</p>
-      <p>i just look "okayish" because i have a good diet and i move around a lot.</p>
-      <p>i only did 1 real workout every week, then the rest was just random cardio throughout the week to get to that physique in the pic above.</p>
-      <p>i chased the surfer physique. its good enough to get attention from girls when i have the shirt off. but with clothes, i look like a boy, skinny. functionally? i look athletic enough, but any time i need to carry load like carry a girl up the stairs or wrestle with a guy, that is where i would struggle.</p>
+      <p>i look meh, poverty abs.</p>
+      <p>thats me barely working out, 2x a week max, but often its more like once a week. just looking "okayish" because i have a good diet and i move around a lot.</p>
+      <p>often just 1 real workout every week, then the rest was just random cardio throughout the week to get to that physique in the pic above.</p>
+      <p>i chased the surfer physique. its good enough to get attention from girls when i have the shirt off. but with clothes on, i look like a boy, skinny. functionally? i look athletic enough, but any time i need to carry load like carry a girl up the stairs or wrestle with a guy, that is where i would struggle.</p>
       <p>there is so much physical improvements left on the table.</p>
-      <p>looks isnt everything, but being jacked is a good signal for real athleticness. enough about having a surfer physique, lets get jacked.</p>
+      <p>looks isnt everything, but being jacked is a good signal for real athleticness. so enough about having a surfer physique, lets get jacked.</p>
       <p>today, march 5, 2026, i decided to lock in on a workout program and take my nutrition a bit more seriously, first going on a lean bulk for several months then cut by around the end of the year.</p>
       <p>for the after photo, i'll update this post by the end of the year.</p>
       <p>lets win.</p>
@@ -30,56 +30,67 @@ const entry: LifemaxxEntry = {
 
     <h2>weights training</h2>
     <p>Long term consistency trumps short term intensity --  Bruce Lee</p>  
-    <h3>strength block</h3>
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-    <h3>hypertrophy block</h3>
+    <p>imo the best weight training is the one that'll get you consistent. engaging in researching whats the most optimal workout to look "shredded", or "balanced", or a guide to look like some lame "alpha male" tiktok bros admire will just lead you down to a rabbithole of rumination instead of execution</p>
+    <p>look at my physique on the pic above, i got there from working out just 1-2x a week simply by increasing the weights. progressive overload over months and eating enough protein was enough. consistency works.</p>
     <section>
     <ul>
-      <li>
-      Day 1: Squat 4x5
-      </li>
-      <li>
-      Day 2: 
-      </li>
-      <li>
-      Day 3:
-      </li>
-      <li>
-      Day 4:
-      </li>
+      <li>Monday</li>
+      <ul>
+        <li>Squat 4x5</li>
+        <li>Chest Supported Rows 5x8</li>
+      </ul>
+      <li>Tuesday</li>
+       <ul>
+        <li>Incline Bench Press 4x5</li>
+        <li>Pullups 3xAMRAP</li>
+        <li>Deadlift 1x5</li>
+      </ul>
+       <li>Thursday</li>
+      <ul>
+        <li>Squat 4x5</li>
+        <li>Chest Supported Rows 5x8</li>
+      </ul>
+      <li>Friday</li>
+       <ul>
+        <li>Incline Bench Press 4x5</li>
+        <li>Pullups 3xAMRAP</li>
+        <li>Deadlift 1x5</li>
+      </ul>
+      <li>Weakpoints</li>
+      <ul>
+        <li>Hit weakpoints like shoulders/delts, neck, abs, whatever you want randomly throughout the week</li>
+      </ul>
     </ul>
     </section>
-
-
-i follow Zulu template from tacticalbarbell
-
-each zulu workout is like 20-30 minutes, i lift at home before work
-
-- Monday: Bench Press 3x5, Weighted Chinup 3x5
-- Tuesday: Squat 3x5, OHP 3x5, Farmer Carry 3x60 seconds
-- Thursday: Incline Bench Press 4x8, Deadlift 1-3x5
-- Friday: Squat 3x5, DB Chest Supported Rows 4x8
-
-traning: bjj 2x, judo 2x
-
-random accessories throughout the week
-
-current physique after 10 months of above
-
 
     
     <h2>athleticism</h2>
     <section>
-      <p>i dont believe in "functional fitness" trends that try to make you look athletic. imo if you want to be athletic, then just pick up a real activity: boxing, MMA, surfing, rowing, BJJ, rock climbing or indoor bouldering, hiking, frisbee, swimming, basketball. thats imo how you develop athleticism</p>
+      <p>i dont believe in "functional fitness" trends that try to make you look athletic. imo if you want to be athletic, then just pick up a real activity:</p>
+      <ol>
+        <li>tennis</li>
+        <li>surfing</li>
+        <li>boxing</li>
+        <li>running or hybrid athlete/OCR type races</li>
+        <li>rock climbing or indoor bouldering</li>
+        <li>golfing</li>
+        <li>hiking</li>
+        <li>swimming</li>
+        <li>rowing or dragonboat racing</li>
+        <li>BJJ/brazilian jiu jitsu</li>
+      </ol>
+      <p>the above are ranked by what girls tell me are attractive/keep telling me to pick up, opportunities available to meet friends, and ones I actually like. such as BJJ is a good third place, but others like a bouldering gym just absolutely decimates it in terms of expanding your social circle and having a personal tribe that'll support your career and personal growth</p>
+      <p>tennis continuously is at the top of the list of sports that girls i've met from various countries keep telling me as a sport they'd like to learn. and lately, ive now started seeing combat sport fitness coaches started recommending it too, for the purpose of having <b>modality</b>. tennis teaches you things you wont see while training judo or jiu jitsu, things like acceleration and deceleration, explosiveness while being elastic, tendon + feet + calves work that become important as you age, the coordination to catch and chase an object</p>
+      <img src="https://pbs.twimg.com/media/HHMkQGVakAAC3RT?format=jpg&name=medium" height="300">
+      <p>tennis = youth. being springy is sex appeal, and its a fun way to awaken the human instinct to throw, track, chase, intercept, and smash an object - which in this case, is flying at a speed of over 100 kilometers an hour.</p>
+      <p>i highly recommend adding it to your training</p>
+      <p>be multimodal. especially today where the trend is people becomming multi-dimensional, like hybrid athletes. the world has rejected the one stat athlete, AI has killed the pursuit of the PhD and given rise to people who can execute multi capabilities, like the cardiologist (Michał Nedoszytko MD, PhD) who can code and beat 13,000+ developers at a recent hackathon by Anthropic. it is <a href="https://www.youtube.com/watch?v=kf77tEkkzvE">the age of the multi-dimensionally jacked</a></p>
+      <p>start by sports-maxxing</p>
     </section>
 
     <h2>you need to get jacked</h2>
     <section>
-      <p>on the day of writing this post, paul graham, published an essay called brand age. and with it, it made me think about the importance of marketing in the AI era.</p>
+      <p>on the day of writing this post, paul graham, published an essay called <a href="https://paulgraham.com/brandage.html">the brand age</a>. and with it, it made me think about the importance of signalling yourself in the AI era.</p>
       <p>skill alone is no longer the threshold when information is cheap and automated knowledge workers (AI agents) makes output hard to differentiate.</p>
       <p>you need = competence + proof + identity.</p>
       <p>the market has shifted from:</p>
@@ -90,11 +101,11 @@ current physique after 10 months of above
       <ul>
         <li>dating arena</li>
         <li>career/business competence</li>
-        <li>online identity</li>
+        <li>online and irl identity</li>
         <li>self-image</li>
       </ul>
-      <p>having a good physique sharpens the face and makes you more energetic, more focused during business deals and long meetings, ready to go on a moment's notice. you become someone.</p>
-      <p>you need to get jacked NOW.</p>
+      <p>having a good physique sharpens the face and makes you more energetic, gives you the vitality to be more focused during business deals and long meetings, ready to execute on a moment's notice. you become someone that looks dependable, a powerful feature for a low trust society.</p>
+      <p>you need to get jacked NOW</p>
       <img src="https://substackcdn.com/image/fetch/$s_!D_eq!,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F963b4e56-3293-4035-91c6-7e3b9ccfc102_1200x675.jpeg">
     </section>
   `,
